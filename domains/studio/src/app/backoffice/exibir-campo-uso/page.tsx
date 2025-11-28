0891eb91341a57fe5293c91943c773119c2b1295
fabioeducacross <fabio.alves@educacross.com.br>
2025-11-28 18:16:57 +0000

    fix(studio): corrige caminho de importação da página exibir-campo-uso

--- domains/studio/src/app/backoffice/exibir-campo-uso/page.tsx
+++ domains/studio/src/app/backoffice/exibir-campo-uso/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import ExibirCampoUsoPage from '../../../../BackOffice/journeys/exibir-campo-uso/page';
+import ExibirCampoUsoPage from '../../../../../BackOffice/journeys/exibir-campo-uso/page';
 
 export default function Page() {
   return <ExibirCampoUsoPage />;
